4bb25e0863ea9f4cebbd568ed72d738776a78632
ChadMWatts <chadmwatts@gmail.com>
2016-09-23 09:55:18 -0600

    made files for views and cells

---
+++ RC2/RC2/PersonalRankTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonalRankTableViewCell.swift
+//  RC2
+//
+//  Created by Chad Watts on 9/23/16.
+//  Copyright © 2016 Chris Yoo. All rights reserved.
+//
+
+import UIKit
+
+class PersonalRankTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RC2/RC2/PublicRankTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PublicRankTableViewCell.swift
+//  RC2
+//
+//  Created by Chad Watts on 9/23/16.
+//  Copyright © 2016 Chris Yoo. All rights reserved.
+//
+
+import UIKit
+
+class PublicRankTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
